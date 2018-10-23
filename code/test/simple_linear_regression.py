b62ae0b956aa810e6838537e973857ec9f5b8b46
Debrup Chakraborty <chakrabd@ITSERV.SCSS.TCD.IE>
2018-10-23 18:42:23 +0100

    Added comments to explain the steps

--- code/test/simple_linear_regression.py
+++ code/test/simple_linear_regression.py
@@ -12,20 +12,36 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression  
 from sklearn import metrics
-data = pd.read_csv('..\..\imdb_1000.csv')
+from sklearn.preprocessing import LabelEncoder
+
+#Loading the dataset using pandas
+data = pd.read_csv('../../dataset/imdb_1000.csv')
 data.shape
 data.head()
 data.describe()
+#Category values need to be changed to numerical codes for applying any ML algorithm.
+lb_make = LabelEncoder()
+data["genre_code"] = lb_make.fit_transform(data["genre"])
+data[["genre", "genre_code"]].head(11)
+#Initial testing includes one predictor (X) that is 'duration' and our target is 'star_rating'
 X=data[['duration']]
 y=data[['star_rating']]
+#Using the sklearn in-built train-test splitting method to divide the data. Random state '0' or any number indicates it will always split the data into outputs that would be same,
+#if it is run multiple times.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#initialising the Linear Regression model constructor
 regressor = LinearRegression()  
+#Train the model with the input data available
 regressor.fit(X_train, y_train)
+#printing various features of the model to know the quality of the model
 print(regressor.intercept_)
 print(regressor.coef_)
+#predicting using the test dataset
 y_pred = regressor.predict(X_test)
+#metric to find the performance of the model
 df = pd.DataFrame({'Actual': [y_test], 'Predicted': [y_pred]})
 df
+#finding standard errors in the model
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
